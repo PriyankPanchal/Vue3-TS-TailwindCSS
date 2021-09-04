acef58da0a59453bddb847aa38d7ce5a91165f9e
development0261 <development0261@gmail.com>
2021-09-04 11:49:28 +0530

    initial commit.

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>  
+  <div class="grid grid-cols-12">
+    <div class="col-span-4">
+      <div class="justify-center flex bg-yellow-300 items-start">
+        <div class="text-sm">
+          Hello 👋🏼
+        </div>
+      </div>
+    </div>
+    <div class="col-span-4">
+      <div class="justify-center flex bg-yellow-300 items-start">
+        <div class="text-sm">
+          Hello 👋🏼
+        </div>
+      </div>
+    </div>
+    <div class="col-span-4">
+      <div class="justify-center flex bg-yellow-300 items-start">
+        <div class="text-sm">
+          Hello 👋🏼
+        </div>
+      </div>
+    </div>
+    <div class="col-span-4">
+      <div class="justify-center flex bg-yellow-300 items-start">
+        <div class="text-sm">
+          Hello 👋🏼
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <router-view /> -->
+</template>
+
+<style></style>
